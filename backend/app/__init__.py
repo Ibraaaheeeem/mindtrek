d96fed917964750eda0e5200eee2f33c18817cf1
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-09-06 17:40:08 +0100

    Create app init with db and migration

---
+++ backend/app/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Initialize SQLAlchemy and Migrate instances without app context
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configure database URL from environment variable
+    database_url = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
+    # Initialize db and migrate with the app
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
