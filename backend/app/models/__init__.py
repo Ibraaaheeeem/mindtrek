3fbd1aac087a58eb01b96460721cff89d2577927
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-09-07 14:22:35 +0100

    Fix bug to correct table relationships

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,3 @@
 from .categories import Category, Subcategory, Subject, Unit
 from .question import Question
-from .social import User, Comment, Attempt, ExamSubjects, MockSubject
+from .social import User, Comment, Attempt, MockSubject
--- backend/app/models/social.py
+++ backend/app/models/social.py
@@ -1,6 +1,12 @@
 from app import db
 from datetime import datetime
 
+exam_subjects = db.Table('exam_subjects',
+    db.Column('attempt_id', db.Integer, db.ForeignKey('exam_attempts.id'), primary_key=True),
+    db.Column('subject_id', db.Integer, db.ForeignKey('mock_subjects.id'), primary_key=True),
+    db.Column('score', db.Float, default=0.0)
+)
+
 class Comment(db.Model):
     __tablename__ = 'comments'
 
@@ -21,12 +27,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     registration_date = db.Column(db.DateTime, default=datetime.utcnow)
     last_seen_date = db.Column(db.DateTime, default=datetime.utcnow)
-
     attempts = db.relationship('Attempt', back_populates='user')
-    attempts = db.relationship('Comment', back_populates='user')
+    comments = db.relationship('Comment', back_populates='user')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -39,6 +44,9 @@ class MockSubject(db.Model):
     num_questions = db.Column(db.Integer, nullable=False)
     score = db.Column(db.Float, nullable=False)
     
+    attempts_taken = db.relationship('Attempt', secondary='exam_subjects',
+                                     back_populates='subjects_taken')
+    
     def __repr__(self):
         return f'<MockSubject {self.name}>'
     
@@ -46,25 +54,13 @@ class Attempt(db.Model):
     __tablename__ = 'exam_attempts'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     duration = db.Column(db.Integer, nullable=False)  # Duration of the exam in minutes
     date_taken = db.Column(db.DateTime, default=datetime.utcnow)
     total_score = db.Column(db.Float, default=0.0)
     total_possible_score = db.Column(db.Float, default=0.0)
-
+    
+    user = db.relationship('User', back_populates='attempts')
     # Define a many-to-many relationship between Attempt and MockSubject
     subjects_taken = db.relationship('MockSubject', secondary='exam_subjects',
                                      back_populates='attempts_taken')
-
-class ExamSubjects(db.Model):
-    __tablename__ = 'exam_subjects'
-
-    id = db.Column(db.Integer, primary_key=True)
-    attempt_id = db.Column(db.Integer, db.ForeignKey('exam_attempts.id'), nullable=False)
-    subject_id = db.Column(db.Integer, db.ForeignKey('mock_subjects.id'), nullable=False)
-    score = db.Column(db.Float, default=0.0)
-
-    # Define many to many relationships
-    attempt = db.relationship('Attempt', back_populates='exam_subjects')
-    subject = db.relationship('MockSubject', back_populates='exam_subjects')
-    
