bac6abbb3966126cd77a3e0c70494bc422256106
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-09-09 11:19:41 +0100

    Fix bugs relted to data types

--- backend/app/models/social.py
+++ backend/app/models/social.py
@@ -1,11 +1,6 @@
 from app import db
 from datetime import datetime
 
-exam_subjects = db.Table('exam_subjects',
-    db.Column('attempt_id', db.Integer, db.ForeignKey('exam_attempts.id'), primary_key=True),
-    db.Column('subject_id', db.Integer, db.ForeignKey('mock_subjects.id'), primary_key=True),
-    db.Column('score', db.Float, default=0.0)
-)
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -42,8 +37,7 @@ class MockSubject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     num_questions = db.Column(db.Integer, nullable=False)
-    score = db.Column(db.Float, nullable=False)
-    
+    score = db.Column(db.Integer, default=0)
     attempts_taken = db.relationship('Attempt', secondary='exam_subjects',
                                      back_populates='subjects_taken')
     
@@ -51,16 +45,28 @@ class MockSubject(db.Model):
         return f'<MockSubject {self.name}>'
     
 class Attempt(db.Model):
-    __tablename__ = 'exam_attempts'
+    __tablename__ = 'attempts'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     duration = db.Column(db.Integer, nullable=False)  # Duration of the exam in minutes
     date_taken = db.Column(db.DateTime, default=datetime.utcnow)
-    total_score = db.Column(db.Float, default=0.0)
-    total_possible_score = db.Column(db.Float, default=0.0)
+    total_score = db.Column(db.Integer, default=0)
+    total_possible_score = db.Column(db.Integer, default=0)
     
     user = db.relationship('User', back_populates='attempts')
     # Define a many-to-many relationship between Attempt and MockSubject
     subjects_taken = db.relationship('MockSubject', secondary='exam_subjects',
                                      back_populates='attempts_taken')
+    
+    def add_mock_subject(self, mock_subject):
+        """
+        Add a MockSubject to the subjects_taken relationship.
+        """
+        self.subjects_taken.append(mock_subject)
+
+exam_subjects = db.Table('exam_subjects',
+    db.Column('attempt_id', db.Integer, db.ForeignKey('attempts.id'), primary_key=True),
+    db.Column('subject_id', db.Integer, db.ForeignKey('mock_subjects.id'), primary_key=True),
+    db.Column('score', db.Integer, default=0)
+)
