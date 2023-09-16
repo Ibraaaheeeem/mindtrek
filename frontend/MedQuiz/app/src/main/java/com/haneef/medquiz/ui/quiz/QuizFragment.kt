56a535ee1f7683cd4fc05348f4af6e0ea2d6d509
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-09-16 06:58:54 +0100

    Remove unused template code

--- frontend/MedQuiz/app/src/main/java/com/haneef/medquiz/ui/quiz/QuizFragment.kt
+++ frontend/MedQuiz/app/src/main/java/com/haneef/medquiz/ui/quiz/QuizFragment.kt
@@ -1,4 +1,4 @@
-package com.haneef.medquiz.ui.slideshow
+package com.haneef.medquiz.ui.quiz
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.haneef.medquiz.databinding.FragmentSlideshowBinding
 
-class SlideshowFragment : Fragment() {
+class QuizFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
 
@@ -22,16 +22,10 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
 
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
