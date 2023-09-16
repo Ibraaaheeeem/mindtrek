354de65e3e5f9fdf256221fb66642102e503c5eb
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-09-16 06:49:54 +0100

    Create data classes for UserData(for Registration and Login) and URLResponse(from backend)

---
+++ frontend/MedQuiz/app/src/main/java/com/haneef/medquiz/data/Response.kt
@@ -0,0 +1,3 @@
+package com.haneef.medquiz.data
+
+data class BackendResponse(val message: String, val access_token: String)
---
+++ frontend/MedQuiz/app/src/main/java/com/haneef/medquiz/data/UserData.kt
@@ -0,0 +1,3 @@
+package com.haneef.medquiz.data
+
+data class UserData(val username: String, val password: String, val email: String)
