d8f4f44f91792b6344e4b4dc7ca27e263545d93f
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-09-16 06:45:39 +0100

    Create class to handle registration

---
+++ frontend/MedQuiz/app/src/main/java/com/haneef/medquiz/utils/RegistrationService.kt
@@ -0,0 +1,25 @@
+import com.google.gson.Gson
+import com.haneef.medquiz.data.UserData
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import java.io.IOException
+
+class RegistrationService(url: String) {
+
+    private val client = OkHttpClient()
+
+    // Replace with your registration endpoint URL
+    private val registrationUrl = url
+
+    fun registerUser(userData: UserData, callback: Callback) {
+        val gson = Gson()
+        val json = gson.toJson(userData)
+        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json)
+        val request = Request.Builder()
+            .url(registrationUrl)
+            .post(requestBody)
+            .header("Content-Type", "application/x-www-form-urlencoded") // Replace with the appropriate content type if needed
+            .build()
+        client.newCall(request).enqueue(callback)
+    }
+}
